9cda5ac06b3d0fd658e61f2219df75f448e1c92c
tarik310 <tareq310harh@gmail.com>
2025-03-09 23:21:16 +0300

    feat: add product expense visualization to Row3 component with scatter chart

--- src/scenes/dashboard/Row3.tsx
+++ src/scenes/dashboard/Row3.tsx
@@ -1,6 +1,6 @@
 import BoxHeader from "@/components/BoxHeader";
 import DashboardBox from "@/components/DashboardBox";
-import { useGetKpisQuery } from "@/state/api";
+import { useGetKpisQuery, useGetProductsQuery } from "@/state/api";
 import { useTheme } from "@mui/material";
 import React, { useMemo } from "react";
 import {
@@ -11,12 +11,16 @@ import {
   XAxis,
   YAxis,
   Line,
+  Scatter,
+  ScatterChart,
+  ZAxis,
+  Label,
 } from "recharts";
 
 const Row3 = () => {
   const { palette } = useTheme();
   const { data: operationalData } = useGetKpisQuery();
-
+  const { data: productData } = useGetProductsQuery();
   const operationalExpenses = useMemo(() => {
     return (
       operationalData &&
@@ -31,18 +35,29 @@ const Row3 = () => {
       )
     );
   }, [operationalData]);
-
+  const productExpenseData = useMemo(() => {
+    return (
+      productData &&
+      productData.map(({ _id, price, expense }) => {
+        return {
+          id: _id,
+          price: price,
+          expense: expense,
+        };
+      })
+    );
+  }, [productData]);
   return (
     <>
       <DashboardBox gridArea="f">
-        <BoxHeader title="Operational vs Non-Operational Expenses" sideText="+4%" />
+        <BoxHeader title="Operational vs Non-Operational Expenses" sideText="+4%???" />
         <ResponsiveContainer width="100%" height="100%">
           <LineChart
             data={operationalExpenses}
             margin={{
               top: 20,
               right: 0,
-              left: -10,
+              left: 0,
               bottom: 55,
             }}
           >
@@ -54,6 +69,13 @@ const Row3 = () => {
               tickLine={false}
               axisLine={false}
               style={{ fontSize: "10px" }}
+              tickFormatter={(value) =>
+                value >= 1_000_000
+                  ? `${value / 1_000_000}M`
+                  : value >= 1_000
+                  ? `${value / 1_000}k`
+                  : value
+              }
             />
             <YAxis
               yAxisId="right"
@@ -61,6 +83,13 @@ const Row3 = () => {
               tickLine={false}
               axisLine={false}
               style={{ fontSize: "10px" }}
+              tickFormatter={(value) =>
+                value >= 1_000_000
+                  ? `${value / 1_000_000}M`
+                  : value >= 1_000
+                  ? `${value / 1_000}k`
+                  : value
+              }
             />
             <Tooltip />
             <Line
@@ -76,6 +105,54 @@ const Row3 = () => {
               stroke={palette.primary.main}
             />
           </LineChart>
+        </ResponsiveContainer>
+      </DashboardBox>
+      <DashboardBox gridArea="g">
+        <BoxHeader title="Product Prices vs Expenses" sideText="+4%???" />
+        <ResponsiveContainer width="100%" height="100%">
+          <ScatterChart
+            margin={{
+              top: 20,
+              right: 25,
+              bottom: 40,
+              left: -10,
+            }}
+          >
+            <CartesianGrid stroke={palette.grey[800]} />
+            <XAxis
+              type="number"
+              dataKey="price"
+              name="price"
+              axisLine={false}
+              tickLine={false}
+              style={{ fontSize: "10px" }}
+              tickFormatter={(v) => `$${v}`}
+            >
+              <Label value="Price" position="top" />
+            </XAxis>
+            <YAxis
+              type="number"
+              dataKey="expense"
+              name="expense"
+              axisLine={false}
+              tickLine={false}
+              style={{ fontSize: "10px" }}
+              tickFormatter={(value) =>
+                value >= 1_000_000
+                  ? `$${value / 1_000_000}M`
+                  : value >= 1_000
+                  ? `$${value / 1_000}k`
+                  : `$${value}`
+              }
+            />
+            <ZAxis type="number" range={[15]} />
+            <Tooltip formatter={(v) => `$${v}`} />
+            <Scatter
+              name="Product Expense Ratio"
+              data={productExpenseData}
+              fill={palette.tertiary[500]}
+            />
+          </ScatterChart>
         </ResponsiveContainer>
       </DashboardBox>
     </>
