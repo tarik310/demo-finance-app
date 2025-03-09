2b0dee891a971d744069052438c5c76d835e4c8a
tarik310 <tareq310harh@gmail.com>
2025-03-09 14:56:12 +0300

    refactor Navbar component: enhance layout structure and improve styling for better alignment and responsiveness

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -8,43 +8,51 @@ const Navbar = () => {
   const { palette } = useTheme();
   const [selected, setSelected] = useState("dashboard");
   return (
-    <FlexBetween mb="0.25rem" p="0.5rem 0rem" color={palette.grey[100]}>
-      {/* LEFT SIDE */}
-      <FlexBetween gap="0.75rem">
-        <PredictiveInsightsLogo />
-        <Typography variant="h4" fontSize="16px">
-          Predictive Insights
-        </Typography>
-      </FlexBetween>
-
-      {/* RIGHT SIDE */}
-      <FlexBetween gap="2rem">
-        <Box sx={{ "&:hover": { color: palette.primary[600] } }}>
-          <Link
-            to="/"
-            onClick={() => setSelected("dashboard")}
-            style={{
-              color: selected === "dashboard" ? "inherit" : palette.grey[500],
-              textDecoration: "inherit",
-            }}
-          >
-            Dashboard
-          </Link>
+    <>
+      <Box mb="0.25rem" p="0.5rem 0rem" sx={{ display: "flex", gap: "1rem" }}>
+        <FlexBetween gap="0.75rem">
+          <PredictiveInsightsLogo />
+          <Typography variant="h4" fontSize="16px">
+            Predictive Insights
+          </Typography>
+        </FlexBetween>
+        <Box
+          sx={{
+            display: "flex",
+            gap: "1rem",
+            flex: 1,
+            borderLeft: `2px solid ${palette.grey[100]}`,
+            alignItems: "center",
+            paddingLeft: "1rem",
+          }}
+        >
+          <Box sx={{ "&:hover": { color: palette.primary[600] } }}>
+            <Link
+              to="/"
+              onClick={() => setSelected("dashboard")}
+              style={{
+                color: selected === "dashboard" ? "inherit" : palette.grey[500],
+                textDecoration: "inherit",
+              }}
+            >
+              Dashboard
+            </Link>
+          </Box>
+          <Box sx={{ "&:hover": { color: palette.primary[600] } }}>
+            <Link
+              to="/predictions"
+              onClick={() => setSelected("predictions")}
+              style={{
+                color: selected === "predictions" ? "inherit" : palette.grey[500],
+                textDecoration: "inherit",
+              }}
+            >
+              Predictions
+            </Link>
+          </Box>
         </Box>
-        <Box sx={{ "&:hover": { color: palette.primary[600] } }}>
-          <Link
-            to="/predictions"
-            onClick={() => setSelected("predictions")}
-            style={{
-              color: selected === "predictions" ? "inherit" : palette.grey[500],
-              textDecoration: "inherit",
-            }}
-          >
-            Predictions
-          </Link>
-        </Box>
-      </FlexBetween>
-    </FlexBetween>
+      </Box>
+    </>
   );
 };
 
