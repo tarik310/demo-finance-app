2e5b32b7681c5e8602a1de154df879376f336a9a
tarik310 <tareq310harh@gmail.com>
2025-03-09 10:36:56 +0300

    add project files

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// });
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+import path from "path";
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
 });
